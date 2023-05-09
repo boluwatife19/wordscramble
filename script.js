36a489c06cbdc35daece3f4a71b7aa1076873bfb
Boluwatife119 <aregpaul@gmail.com>
2023-05-09 22:55:49 +0500

    first com

--- script.js
+++ script.js
@@ -6,6 +6,60 @@ const words = [
     {
         word: 'pocket',
         hint: 'A bag for carrying small items'
+    },{
+        word: 'treasure ',
+        hint: 'Valuable or precious possessions hidden or lost'
+    },{
+        word: 'mysterious ',
+        hint: 'Difficult to understand or explain'
+    },{
+        word: 'victory ',
+        hint: 'Triumph or success in a competition or battle'
+    },{
+        word: 'telescope ',
+        hint: ' Instrument for viewing distant objects in space'
+    },{
+        word: 'cathedral ',
+        hint: ' Large and important Christian church'
+    },{
+        word: 'algorithm ',
+        hint: 'Step-by-step procedure for solving a problem'
+    },{
+        word: 'symphony ',
+        hint: 'Elaborate musical composition for an orchestra'
+    },{
+        word: 'diamond ',
+        hint: ' Precious gemstone known for its brilliance'
+    },{
+        word: 'novel ',
+        hint: ' Long fictional narrative book'
+    },{
+        word: 'galaxy ',
+        hint: 'A bag for carrying small items'
+    },{
+        word: 'galaxy ',
+        hint: 'System of stars, gas, and dust in outer space'
+    },{
+        word: 'oasis ',
+        hint: 'A fertile spot in a desert with water'
+    },{
+        word: 'unicorn ',
+        hint: 'Mythical creature with a single horn'
+    },{
+        word: 'recipe  ',
+        hint: 'Set of instructions for preparing a dish'
+    },{
+        word: 'adventure ',
+        hint: 'Exciting or daring experience'
+    },{
+        word: 'camera ',
+        hint: 'Device for capturing photographs or videos'
+    },{
+        word: 'guitar ',
+        hint: 'Musical instrument with strings'
+    },{
+        word: 'keyboard ',
+        hint: 'Input device for typing characters'
     },
     {
         word: 'expert',
